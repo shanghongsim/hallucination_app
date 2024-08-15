f9ab817bfe7601c957c0463181fae9addf647cc2
shanghongsim <shanghong_sim@mymail.sutd.edu.sg>
2024-08-15 18:59:34 +0800

    finalised questions

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+from datetime import datetime
+import csv
 
 # Initialize Firebase Admin SDK
 def initialize_firebase():
-    import firebase_admin
-    from firebase_admin import credentials, firestore
-    from datetime import datetime
     # cred = credentials.Certificate("/home/maojia/work/hallucination_app/hallucination-human-eval-firebase-adminsdk-u77sb-82eeb9f363.json")
     service_account_info = {
         "type": st.secrets["firebase"]["type"],
@@ -19,11 +21,8 @@ def initialize_firebase():
         "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
     }
     if not firebase_admin._apps:
-        # Path to the service account key JSON file
         cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred)
-
-    # Initialize Firestore
     return firestore.client()
 
 # Initialize Firestore
@@ -31,27 +30,20 @@ db = initialize_firebase()
 
 def escape_markdown(text):
     import re
-    # Define the characters to escape
     escape_chars = r'([\[\](){}*+?.\\^$|#])'
-    
-    # Use regex to escape all the special characters
     return re.sub(escape_chars, r'\\\1', text)
 
 def load_data():
-    import json
-    # Load the data from JSON
-    with open('human_eval_data.json', 'r', encoding='utf-8') as f:
+    with open('output.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     return data
 
-def save_responses_to_firestore(responses, form_number, code, name):
-    import csv
-    from datetime import datetime
-    
-    # Reference to the Firestore collection
-    collection_ref = db.collection(f'form_{name}_responses')
+def split_into_chunks(lst, chunk_size):
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
 
-    # Save each response as a document
+def save_responses_to_firestore(responses, form_number, code, name):
+    collection_ref = db.collection(f'form_{name}_responses')
     for question, response in responses.items():
         doc_ref = collection_ref.document()
         doc_ref.set({
@@ -73,23 +65,28 @@ code_to_form = {
 
 def main():
     st.set_page_config(
-    page_icon="🚀",
+    page_icon="🔬",
     layout="wide",
     initial_sidebar_state="expanded",
 )
-    st.title("Human Evaluation: Appropriateness of Rejection")
+    st.title("Hallucination Human Evaluation")
 
     multi = """
     Thank you for participating in this human evaluation! Please review the instructions before beginning:
 
-    1. This survey consists of 15 questions and should take no more than 30 minutes to complete.
-    2. Please enter your name and the access code provided by Shang Hong in the fields below.
-    3. You will be asked to rate whether the model's output is appropriate. Use the following scale to rate each item:
+    1. This survey includes 8 samples. It should take no more than 30 minutes to complete.
+    2. For each sample, you will see 2 answers from different models. Your task is to rate each answer based on how well it addresses the question using the provided documents.
+    3. Use the following scale to rate each item:
+    
         **5** – Strongly Agree  
         **4** – Agree  
         **3** – Neutral  
         **2** – Disagree  
         **1** – Strongly Disagree
+    4. The answer should be based on the information provided in the documents. If the documents do not have enough information to answer the question, a refusal is a valid response.
+    5. Please enter your name and the access code provided by Shang Hong in the fields below.
+
+    Thank you for your participation!
     """
     st.markdown(multi)
 
@@ -114,56 +111,43 @@ def display_form(form_number, code, name):
     # Display the specific form based on form_number
     st.header(f"Set {form_number}")
 
-    # Example: 60 questions divided into 4 forms
     data = load_data()
-
-    # Split questions into 4 chunks of 15 questions each
-    def split_into_chunks(lst, chunk_size):
-        for i in range(0, len(lst), chunk_size):
-            yield lst[i:i + chunk_size]
-
-    chunk_size = 15
+    chunk_size = 8
     forms = list(split_into_chunks(data, chunk_size))  # List of 4 lists
 
     # Retrieve questions for the selected form
     form_index = form_number - 1  # Adjust for zero-based indexing
     form_questions = forms[form_index]  # List of 15 questions for the selected form
-
-    # Define Likert scale options
-    likert_options = ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
-
-   
-    # Create a form in Streamlit
     with st.form(key=f'form_{form_number}'):
         responses = {}
-         # Loop through each sample in the data
         for i, sample in enumerate(form_questions):
-            st.subheader(f"Sample {i + 1}: Given the following questions and documents, do you think the model's rejection is appropriate for Sample {i + 1}?")
-            
-            # Display the question
+            st.subheader(f"Sample {i + 1}: Please read the sample and answer the following questions carefully.")
             st.markdown(f"**Question:** \"{escape_markdown(sample['question'])}\"")
-
-            # Display the documents
             st.markdown("**Documents:**")
             for idx, doc in enumerate(sample['docs']):
                 st.markdown(f"- **Document [{idx + 1}]** (Title: {escape_markdown(doc['title'])}): {escape_markdown(doc['text'])}")
 
-            # Display the model output
-            st.markdown(f"**Model Output:** \"{escape_markdown(sample['output'])}\"")
+            likert_options = ["5 - Strongly Disagree", "4 - Disagree", "3 - Neutral", "2 - Agree", "1 - Strongly Agree"]
 
-            # Likert scale question
-            likert_options = ["Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"]
-            response = st.radio(f"Please select your answer for Sample {i + 1}:", likert_options, index=None, key=f"response_{i}")
+            # First question
+            st.markdown(f"**Answer 1:** \"{sample['llama3_answer']}\"")
+            # st.markdown("To what extent do you agree that the answer addresses the question using the given documents? The answer should follow the information provided in the documents. Note that if the documents do not contain sufficient information to answer the question, a refusal is a valid response. Please rate your answer on a scale of 1-5:")
+            llama3_answer = st.radio(f"To what extent do you agree that the answer addresses the question using the given documents? The answer should follow the information provided in the documents. Note that if the documents do not contain sufficient information to answer the question, a refusal is a valid response. Please rate your answer on a scale of 1-5:", likert_options, index=None, key=f"response1_{i}")
+
+            # Second question
+            st.markdown(f"**Answer 2:** \"{sample['gpt35_answer']}\"")
+            # st.markdown("")
+            gpt35_answer = st.radio(f"To what extent do you agree that the answer addresses the question using the given documents? The answer should follow the information provided in the documents. Note that if the documents do not contain sufficient information to answer the question, a refusal is a valid response. Please rate your answer on a scale of 1-5:", likert_options, index=None, key=f"response2_{i}")
 
             st.write("---")  # Separator between samples
-            responses[i] = response
+            responses[f"{sample['label']}"] = {"llama3_answer": llama3_answer, "gpt35_answer": gpt35_answer}
 
         # Submit button
         submitted = st.form_submit_button("Submit")
 
         if submitted:
             # Check for unanswered questions
-            unanswered_questions = [q for q, r in responses.items() if r not in likert_options]
+            unanswered_questions = [q for q, r in responses.items() if r is None]
             if unanswered_questions:
                 st.error("Please answer all questions before submitting the form.")
             else:
